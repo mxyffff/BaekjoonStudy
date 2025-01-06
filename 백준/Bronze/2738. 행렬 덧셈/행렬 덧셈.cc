5b9463f35c9247a23b5cf1047c86d856c2662ad2
Dakyeom Lee <122773041+mxyffff@users.noreply.github.com>
2025-01-07 00:56:10 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -9,23 +9,18 @@ int main()
 
 	vector<vector <int>> matrix1(n, vector<int>(m));
 	vector<vector <int>> matrix2(n, vector<int>(m));
-	vector<vector <int>> matrix3(n, vector<int>(m));
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
 			cin >> matrix1[i][j];
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
 			cin >> matrix2[i][j];
-		}
-	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
-			cout << matrix3[i][j] << " ";
+			cout << matrix1[i][j] + matrix2[i][j] << " ";
 		}
 		cout << endl;
 	}
