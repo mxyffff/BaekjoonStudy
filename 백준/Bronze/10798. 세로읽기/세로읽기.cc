d335db933d545fc544c2a27317ff8c696d84d61c
Dakyeom Lee <122773041+mxyffff@users.noreply.github.com>
2025-01-08 11:17:56 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -5,20 +5,24 @@ using namespace std;
 int main()
 {
 	vector<string> words(5);
-	vector<vector <char>> words_2d(5, vector<char> (15, '*'));
 	string sentence;
 
 	for (int i = 0; i < 5; i++)
 		cin >> words[i];
 
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < words[i].size(); j++)
-			words_2d[i][j] = words[i][j];
-			
-	for (int i = 0; i < 15; i++)
-		for (int j = 0; j < 5; j++)
-			if (words_2d[j][i] != '*')
-				sentence += words_2d[j][i];
-	
+	size_t max_length = 0;
+	for (const string& word : words)
+	{
+		if (word.size() > max_length)
+			max_length = word.size();
+	}
+
+	for (size_t i = 0; i < max_length; i++)
+		for (size_t j = 0; j < 5; j++)
+		{
+			if (i < words[j].size())
+				sentence += words[j][i];
+		}
+
 	cout << sentence;
 }
